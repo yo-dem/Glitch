b036b8a3b1d08307994ea508c1c1bb1405c2044e
yodema <yodema.luca@gmail.com>
2024-08-03 09:41:40 +0200

    Sistemata la formattazione di tutti i files

--- movingPlatform.js
+++ movingPlatform.js
@@ -1,24 +1,26 @@
 class MovingPlatform extends Platform {
-	constructor(posX, posY, width, height, speedX, speedY, rangeX, rangeY) {
-		super(posX, posY, width, height);
-		this.startPos = createVector(posX, posY);
-		this.speed = createVector(speedX, speedY);
-		this.range = createVector(rangeX, rangeY);
-		this.prevPosition = createVector(posX, posY);
-	}
+  constructor(posX, posY, width, height, speedX, speedY, rangeX, rangeY) {
+    super(posX, posY, width, height);
+    this.startPos = createVector(posX, posY);
+    this.speed = createVector(speedX, speedY);
+    this.range = createVector(rangeX, rangeY);
+    this.prevPosition = createVector(posX, posY);
+  }
 
-	update() {
-		this.prevPosition.set(this.position);
-		if (this.speed.x !== 0) {
-			this.position.x = this.startPos.x + sin(frameCount * this.speed.x) * this.range.x;
-		}
-		if (this.speed.y !== 0) {
-			this.position.y = this.startPos.y + sin(frameCount * this.speed.y) * this.range.y;
-		}
-	}
+  update() {
+    this.prevPosition.set(this.position);
+    if (this.speed.x !== 0) {
+      this.position.x =
+        this.startPos.x + sin(frameCount * this.speed.x) * this.range.x;
+    }
+    if (this.speed.y !== 0) {
+      this.position.y =
+        this.startPos.y + sin(frameCount * this.speed.y) * this.range.y;
+    }
+  }
 
-	show() {
-		this.update();
-		super.show();
-	}
+  show() {
+    this.update();
+    super.show();
+  }
 }
--- platform.js
+++ platform.js
@@ -1,12 +1,12 @@
 class Platform {
-	constructor(posX, posY, width, height) {
-		this.position = createVector(posX, posY);
-		this.dimension = createVector(width, height);
-	}
+  constructor(posX, posY, width, height) {
+    this.position = createVector(posX, posY);
+    this.dimension = createVector(width, height);
+  }
 
-	show() {
-		rect(this.position.x, this.position.y, this.dimension.x, this.dimension.y);
-	}
+  show() {
+    rect(this.position.x, this.position.y, this.dimension.x, this.dimension.y);
+  }
 
-	update() {}
+  update() {}
 }
